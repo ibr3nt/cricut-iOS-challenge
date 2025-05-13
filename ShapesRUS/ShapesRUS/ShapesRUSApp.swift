08d79c6fbe55b6c5d66df38d9d923a154c9e5b69
Brent Arnold <brent@brentarnold.com>
2025-05-13 16:08:49 -0600

    Initial empty project commit.

---
+++ ShapesRUS/ShapesRUS/ShapesRUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesRUSApp.swift
+//  ShapesRUS
+//
+//  Created by Brent Arnold on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ShapesRUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShapesRUS/ShapesRUSTests/ShapesRUSTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesRUSTests.swift
+//  ShapesRUSTests
+//
+//  Created by Brent Arnold on 5/13/25.
+//
+
+import Testing
+@testable import ShapesRUS
+
+struct ShapesRUSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
